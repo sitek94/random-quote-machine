ea7cdd6c8001715e638ef6c461128d2cb64abf8f
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-15 17:18:07 +0200

    Add fetching random quote functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { random } from 'lodash';
 import './App.css';
 
 const text =
@@ -16,27 +17,31 @@ function App() {
 }
 
 function QuoteMachine() {
-  const [quotes, setQuotes] = useState(null);
+  const [text, setText] = useState('');
+  const [author, setAuthor] = useState('')
 
   useEffect(() => {
-    fetchData();
+    fetchRandomQuote();
   }, []);
 
-  const fetchData = () => {
+  const fetchRandomQuote = () => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setQuotes(data.quotes);
+        const quotes = data.quotes;
+        const randomNum = random(0, quotes.length);
+        const randomQuote = quotes[randomNum];
+
+        setAuthor(randomQuote.author);
+        setText(randomQuote.quote);
       });
   };
-
-  
 
   return (
     <div id="quote-box">
       <div id="text">{text}</div>
-      <div id="author">Author</div>
-      <button id="new-quote">New Quote</button>
+      <div id="author">{author}</div>
+      <button id="new-quote" onClick={fetchRandomQuote}>New Quote</button>
       <a href="twitter.com/intent/tweet" id="tweet-quote">
         Tweet quote
       </a>
