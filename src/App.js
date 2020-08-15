339c91280ea1a6f2ff5c5086604c1e6b9c4e2054
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-15 16:44:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+const text =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci repudiandae ut repellat ullam, facilis molestiae qui voluptatum minus ab debitis. Voluptatibus hic unde laudantium ea? Aspernatur quasi eos doloribus distinctio?';
+
+const url =
+  'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteMachine />
+    </div>
+  );
+}
+
+function QuoteMachine() {
+  const [quotes, setQuotes] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setQuotes(data.quotes);
+      });
+  };
+
+  
+
+  return (
+    <div id="quote-box">
+      <div id="text">{text}</div>
+      <div id="author">Author</div>
+      <button id="new-quote">New Quote</button>
+      <a href="twitter.com/intent/tweet" id="tweet-quote">
+        Tweet quote
+      </a>
     </div>
   );
 }
