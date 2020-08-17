661b973a3c5c299f96851307dd7093d00c435a68
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-17 13:40:52 +0200

    Add all the theme colors

---
+++ src/colors.js
@@ -0,0 +1,36 @@
+import tinycolor from 'tinycolor2';
+
+export const colors = [
+  // Sunflower yellow
+  { ...createColorObject("#FFC312") }, 
+  // Orange
+  { ...createColorObject("#EE5A24") },
+  // Mint green
+  { ...createColorObject("#2ed573") },
+  // Grass green
+  { ...createColorObject("#009432") },
+  // Blue martina
+  { ...createColorObject("#12CBC4") },
+  // Blue marine
+  { ...createColorObject("#0652DD") },
+  // Turqish aqua
+  { ...createColorObject("#006266") },
+  // Lightpurple circumorbital
+  { ...createColorObject("#5758BB") },
+  // Berry
+  { ...createColorObject("#ED4C67") },
+  // Magenta
+  { ...createColorObject("#6F1E51") },
+  // Dark gray
+  { ...createColorObject("#2d3436") },
+  // Red pigment
+  { ...createColorObject("#EA2027") },
+];
+
+function createColorObject(color) {
+  return {
+    main: color,
+    // Darken primary color using TinyColor
+    dark: tinycolor(color).darken(4).toString()
+  };
+}
