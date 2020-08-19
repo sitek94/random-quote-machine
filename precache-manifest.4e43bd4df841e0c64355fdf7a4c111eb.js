83c2e1433a02fa9cc04a8fdd44b694b12f585c96
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-19 10:37:44 +0200

    Updates

---
+++ precache-manifest.4e43bd4df841e0c64355fdf7a4c111eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77602a8517b8d52ccca0d0833c3781e",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "4ecaea13dc3c5c4f27a8",
+    "url": "/random-quote-machine/static/css/main.9a0c0739.chunk.css"
+  },
+  {
+    "revision": "a13ebf632447f36d6725",
+    "url": "/random-quote-machine/static/js/2.22a24620.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/random-quote-machine/static/js/2.22a24620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ecaea13dc3c5c4f27a8",
+    "url": "/random-quote-machine/static/js/main.cbdd9d61.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
